74d2f8f798d431f25f5548379fbac9e57ef4f39d
Aditya <nugroho.aditya@8commerce.com>
2020-07-06 16:27:46 +0800

    version 1 to production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
-
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
 ))->bootstrap();
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
 
-$router->get('/',['as' => 'index','uses' => 'IndexController@index']); 
-$router->get('/page-not-found',['as' => 'pagenotfound','uses' => 'IndexController@pagenotfound']); 
-$router->get('/server-error',['as' => 'servererror','uses' => 'IndexController@servererror']);
-$router->get('/version', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => 'v1'], function () use ($router) {
+	$router->get('/',['as' => 'index','uses' => 'IndexController@index']); 
+	$router->get('/page-not-found',['as' => 'pagenotfound','uses' => 'IndexController@pagenotfound']); 
+	$router->get('/server-error',['as' => 'servererror','uses' => 'IndexController@servererror']);
+	$router->get('/version', function () use ($router) {
+		return $router->app->version();
+	});
+
+	$router->get('/coba',['as' => 'coba','uses' => 'IndexController@coba']); 
+	 
+
+
+	$router->group(['prefix' => 'luxasia', 'namespace' => 'Luxasia'], function () use ($router) {
+		
+		$router->get('/',['as' => 'luxasiaIndex','uses' => 'IndexController@index']);
+		$router->get('sku',['as' => 'luxasiaImportSKU','uses' => 'IndexController@sku']);
+		$router->get('po',['as' => 'luxasiaImportPo','uses' => 'IndexController@po']);
+		$router->get('receipts-po',['as' => 'luxasiaReceiptsPo','uses' => 'IndexController@receiptsPo']);
+		$router->get('stock',['as' => 'luxasiaStock','uses' => 'IndexController@stock']);
+		$router->get('stock-transfer',['as' => 'luxasiaStockTransfer','uses' => 'IndexController@stockTransfer']);
+		$router->get('so',['as' => 'luxasiaSo','uses' => 'IndexController@so']);
+
+	});
 });
-
-$router->get('/coba',['as' => 'coba','uses' => 'IndexController@coba']); 
- 
-
-
-$router->group(['prefix' => 'luxasia', 'namespace' => 'Luxasia'], function () use ($router) {
-	
-    $router->get('/',['as' => 'luxasiaIndex','uses' => 'IndexController@index']);
-    $router->get('sku',['as' => 'luxasiaImportSKU','uses' => 'IndexController@sku']);
-    $router->get('po',['as' => 'luxasiaImportPo','uses' => 'IndexController@po']);
-    $router->get('receipts-po',['as' => 'luxasiaReceiptsPo','uses' => 'IndexController@receiptsPo']);
-    $router->get('stock',['as' => 'luxasiaStock','uses' => 'IndexController@stock']);
-    $router->get('stock-transfer',['as' => 'luxasiaStockTransfer','uses' => 'IndexController@stockTransfer']);
-    $router->get('so',['as' => 'luxasiaSo','uses' => 'IndexController@so']);
-
-});
-
  
